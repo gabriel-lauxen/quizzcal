4b1ec2174d159d46b6ddf19654bcd37a6a7b1061
Gabriel Lauxen <gabriellauxen11@gmail.com>
2023-06-23 18:36:59 -0300

    rendering questions and answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,25 +1,45 @@
 import React, { useEffect, useState } from 'react';
+import {decode} from 'html-entities';
+import { shuffle } from "lodash";
 
 export default function Quiz () {
     
     const [questions,setQuestions] = useState([])
 
-    const getQuestions = () => {
-        fetch('https://opentdb.com/api.php?amount=5').then(r => {
-            return r.json()
-        }).then(data => {
-            setQuestions(data)
-        })
-        
-    }
     useEffect(() => {
-        getQuestions()
-        console.log(questions)
+        fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+        .then(r => {return r.json()})
+        .then(data => {
+            const formattedQuestions = data.results.map(question => {
+                const answers = shuffle([...question.incorrect_answers,question.correct_answer])
+                return {...question,answers,selectedOption: ''}
+            })
+            setQuestions(formattedQuestions)
+        }) 
     },[])
+    
+    console.log(questions)
+
+    const questionsElements = questions.map(question => {
+        return (
+            <div className='question'>
+                <h3 className='question-title' >{decode(question.question,'all')}</h3>
+                <div className='answers-container'>
+                    {question.answers.map(answer => {
+                        return <div className='question-answer'>{decode(answer,'all')}</div>
+                    })}
+                </div>
+
+            </div>
+
+        )
+    })
 
     return (
-        <>
-            <h1>quiz!</h1>
+        <>   
+            <div className='questions-container'>
+                {questionsElements}
+            </div>
         </>
     )
 }
