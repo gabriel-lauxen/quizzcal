ad2dc66c34fd1ec2d005efcfa2aaa1842f229f07
Gabriel Lauxen <gabriellauxen11@gmail.com>
2023-07-05 20:49:56 -0300

    cheking answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,7 +5,9 @@ import { shuffle } from "lodash";
 export default function Quiz () {
     
     const [questions,setQuestions] = useState([])
-
+    const [checked,setChecked] = useState(false)
+    const [correctAnswers,setCorrectAnswers] = useState(0)
+    
     useEffect(() => {
         fetch('https://opentdb.com/api.php?amount=5&type=multiple')
         .then(r => {return r.json()})
@@ -18,15 +20,30 @@ export default function Quiz () {
         }) 
     },[])
     
-    console.log(questions)
 
-    const questionsElements = questions.map(question => {
+    function selectOption(questionIndex,answer) {
+        setQuestions(prevQuestions => {
+            const newQuestions = [...prevQuestions]
+            newQuestions[questionIndex].selectedOption = answer
+            return newQuestions   
+        })
+    }
+
+    const questionsElements = questions.map((question,i) => {
         return (
-            <div className='question'>
+            <div key={question.question} className='question'>
                 <h3 className='question-title' >{decode(question.question,'all')}</h3>
                 <div className='answers-container'>
                     {question.answers.map(answer => {
-                        return <div className='question-answer'>{decode(answer,'all')}</div>
+                        return <div key={answer}
+                                    onClick={() => selectOption(i,answer)}
+                                    className={question.selectedOption === answer ?
+                                        'answer answer-selected' :
+                                        'answer'
+                                    }
+                                >
+                                        {decode(answer,'all')}
+                                </div>
                     })}
                 </div>
 
@@ -35,10 +52,34 @@ export default function Quiz () {
         )
     })
 
+    const checkAnswers = () => {
+        console.log(questions.length)
+        setChecked(true)
+        setCorrectAnswers( () => {
+            const filtered = questions.filter(question => question.correct_answer === question.selectedOption)
+            console.log('corretas', filtered)
+            return filtered.length
+        })
+    }
+
     return (
         <>   
             <div className='questions-container'>
                 {questionsElements}
+                <div className="buttons-container">
+                    {!checked ?
+                
+                    <button onClick={() => checkAnswers()}>
+                        Check answers
+                    </button> :
+                    <>
+                        <h4>You scored {correctAnswers}/{questions.length} correct answers</h4>
+                        <button onClick={() => setChecked(false)}>
+                            Play again
+                        </button>
+                    </>
+                    }
+                </div>
             </div>
         </>
     )
